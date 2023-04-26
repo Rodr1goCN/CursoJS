1b6b9bc9376418a0cf09ecfed5d5b3fe1cc746a3
Rodrigo <123037167+Rodr1goCN@users.noreply.github.com>
2023-04-26 19:18:05 -0300

    console dot log

---
+++ Aula 1/index.js
@@ -0,0 +1,4 @@
+console.log('ola mundo');  //aspas simples: não pode adicionar outras aspas dentro dela. Ex.: 'olá 'mundo'' (só funciona com aspas dulplas. Ex.: 'olá "mundo"')
+console.log("olá 'mundo'"); //aspas duplas: consegue adicionar a aspas simples dentro do texto. Ex.: "olá 'mundo'"
+console.log(`'olá' "mundo"`); //crase funciona tanto com a aspas simples quanto com a aspas duplas
+console.log(123, 15.58, 'olá mundo');  //a virgula "," funciona como um 'espaçador' dentro 
