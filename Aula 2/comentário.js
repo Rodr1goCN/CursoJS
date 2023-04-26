ad62fdb65be05ad1693bfa5988877517324ccf7a
Rodrigo <123037167+Rodr1goCN@users.noreply.github.com>
2023-04-26 19:58:34 -0300

    novas aulas de js

---
+++ Aula 2/comentário.js
@@ -0,0 +1,4 @@
+console.log('olá mundo')//isso é um comentário de linha
+/* isso é um comentário 
+de bloco, usando
+varias linhas */
---
+++ Aula 3/index.js
@@ -0,0 +1,3 @@
+console.log('olá mundo')
+console.log('Este trecho sera exibido no console do navegador')
+alert('olá mundo')
