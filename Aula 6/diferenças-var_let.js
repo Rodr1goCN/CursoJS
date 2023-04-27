ba164d9faaa7e0ffdbe5d89f563a3e545cfe5fac
Rodrigo <123037167+Rodr1goCN@users.noreply.github.com>
2023-04-27 19:41:46 -0300

    novas aulas

---
+++ Aula 6/diferenças-var_let.js
@@ -0,0 +1,5 @@
+var nome = 'rodrigo'
+var nome = 'costa' //podemos redeclarar usando VAR, diferente do LET que não podemos fazer isso
+
+let nome = 'rodrigo'
+nome = 'costa' //jeito certo usando LET
---
+++ Aula 7/dados_primitivos.js
@@ -0,0 +1,10 @@
+//tipos de dados primitivos: String, Number, Null, Undefined, Boolean, Symbol (vamos ver mais para frente)
+
+const nome = 'rodrigo'; //string
+const nome1 = "rodrigo"; //string
+const nome2 = `rodrigo`; //string
+const num1 = 10; //Number
+const num2 = 10.50; //Number
+let nomeAluno; // undefined = não aponta para local nenhum na memória
+let sobrenomeAluno = null; //nulo = não aponta para local nenhum na memória
+const aprovado = false; //Boolean = true ou false (lógico)
