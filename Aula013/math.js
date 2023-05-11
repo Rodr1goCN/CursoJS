5bfaa535da2523384a43266daf1f4917c86d0d26
Rodrigo <123037167+Rodr1goCN@users.noreply.github.com>
2023-05-11 00:32:25 -0300

    nova aula, exercício de IMC e correções de aulas anteriores

--- Aula013/math.js
+++ Aula013/math.js
@@ -1,9 +1,14 @@
 let num1 = 100;
 //let num2 = Math.floor(num1); // o floor arredonda os números com decimais para baixo
+
 //let num2 = Math.ceil(num1); // o ceil arredonda os números com decimais para cima
+
 //let num2 = Math.round(num1); // o round arredonda para o número mais próximo, ex: 9.49 = 9 // 9.51 = 10
+
 // console.log(Math.max(1,5,9,7,3,4,20,30,-50,50,0,300,25,45)); Math.max pega o número maior da sequência
+
 //console.log(Math.min(1,5,9,7,3,4,20,30,-50,50,0,300,25,45)); Math.min pega o menor número da sequência
+
 //console.log(Math.random()); // gera um número aleatório entre 0 e 1 (o 1 nunca é incluído)
 
 let aleatorio = Math.round(Math.random() * (10 - 5) + 2); // ele vai gerar um número aleatório entre 0 e 1 e realizar a multiplicação + a soma e arredondar esse número.
