8fce13bdc2b03b178148580293bed80764415fc4
Rodrigo <123037167+Rodr1goCN@users.noreply.github.com>
2023-05-16 19:19:46 -0300

    atualizando a aula 29

--- Aula029/atribuição_via_desestruturação.js
+++ Aula029/atribuição_via_desestruturação.js
@@ -1,1 +1,21 @@
-const
+const pessoa = {
+nome: 'Rodrigo',
+sobrenome: 'costa',
+idade: '37',
+endereco: {
+    rua: 'Itacolomi',
+    numero: '0'
+}
+};
+
+//Atribuição via desestruturação
+const {nome = '', } = pessoa; //podemos definir outro nome dentro do objeto. ex: nome = 'André'
+
+
+const {nome: teste = 'André', sobrenome, idade, endereco: {rua: r = 12346, numero}} = pessoa
+console.log(teste, sobrenome, idade, r, numero)
+
+//para funcionar o nome: teste = 'André' precisa por {} e fica assim: nome: {teste = 'André'}
+
+const {nomes, ...resto} = pessoa;
+console.log(resto)
