2aafcfd1e9d2b9f8d63c690a668bf9b340526839
Rodrigo <123037167+Rodr1goCN@users.noreply.github.com>
2023-05-19 19:58:02 -0300

    novo exercício

---
+++ Aula037/Ex002.js
@@ -0,0 +1,13 @@
+/* function ePaisagem(largura, altura){
+
+    return largura > altura;
+} */
+
+
+//ARROW FUNCTION
+
+
+const ePaisagem = (largura, altura) => largura > altura;
+
+console.log(ePaisagem(800,600)); 
+console.log(ePaisagem(400,800)) ;
