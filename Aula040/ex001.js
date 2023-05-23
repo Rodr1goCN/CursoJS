2d2af851468cef3e0a45d85578af7ea8105fc3e4
Rodrigo <123037167+Rodr1goCN@users.noreply.github.com>
2023-05-23 18:28:49 -0300

    atualizando a aula 40

--- Aula040/ex001.js
+++ Aula040/ex001.js
@@ -1,41 +1,61 @@
+function criarHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', { timeZone: 'GMT' })
+}
+
 const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
 
-let pausado = false;
 let intervalo; //Variável para armazenar o identificador do intervalo
-let contador = 0
+let contador = 0;
 
+function iniciaRelogio() {
+    intervalo = setInterval(function () {
+        contador++;
+        relogio.innerHTML = criarHoraDosSegundos(contador);
+    }, 1000);
+}
 
 iniciar.addEventListener('click', function (evento) {
-    
-    setInterval(() => {
-        contador++;
-        const hora = String(Math.floor(contador / 3600)).padStart(2, '0');
-        const minuto = String(Math.floor((contador % 3600) / 60)).padStart(2, '0');
-        const segundo = String(contador % 60).padStart(2, '0');
+    relogio.classList.remove('pausado')
+    clearInterval(intervalo);
+    iniciaRelogio();
+})
 
-        const counter = `${hora}:${minuto}:${segundo}`
-        relogio.innerHTML = counter
-    }, 1000);
-});
 pausar.addEventListener('click', function (evento) {
-    function atualizaContador(){
-        let contador = 0
-        contador++
-        relogio.innerHTML = contador
-    }
-    pausado = !pausado
-    if (pausado) {
-
-        clearInterval(intervalo);
-
-    } else {
-        intervalo = setInterval(atualizaContador, 1000);
-    }
+    relogio.classList.add('pausado')
+    clearInterval(intervalo);
 });
 zerar.addEventListener('click', function (evento) {
-    alert('Cliquei')
+    relogio.innerHTML = "00:00:00";
+    clearInterval(intervalo);
+    contador = 0;
+    relogio.classList.remove('pausado')
 });
 
+/* Podemos tirar o addEventListener e por só um, ex:
+
+document.addEventListener('click', function(e) {
+    const el = e.target;
+if (el.classList.contains('zerar')) {
+      clearInterval(timer);
+      relogio.innerHTML = '00:00:00';
+      relogio.classList.remove('pausado');
+      segundos = 0;
+    }
+
+    if (el.classList.contains('iniciar')) {
+      relogio.classList.remove('pausado');
+      clearInterval(timer);
+      iniciaRelogio();
+    }
+
+    if (el.classList.contains('pausar')) {
+      clearInterval(timer);
+      relogio.classList.add('pausado');
+    }
+  });
+}
+*/
