4d94eb24526ba8e7fd6ff7a46d994c16d5e42359
Rodrigo <123037167+Rodr1goCN@users.noreply.github.com>
2023-05-29 19:40:58 -0300

    função construtora

---
+++ Aula051/função_construtora.js
@@ -0,0 +1,25 @@
+//função construtora -> objetos
+//função fábrica -> objetos
+//Construtora -> Pessoa (new)
+
+function Pessoa(nome,sobrenome){
+    //atributos ou métodos privados
+    const ID = 123456;
+    const metodoInterno = functio(){
+
+    };
+
+    //atributos ou métodos públicos
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.metodo = function(){
+        console.log(this.nome + ': sou um método');
+    }
+}
+
+
+
+const p1 = new Pessoa('Rodrigo', 'Costa'); //obrigatório usar o new e o Pessoa (P = maiúsculo)
+const p2 = new Pessoa('Maria', 'Silva');//new -> cria um objeto vazio, faz o this apontar para este objeto vazio e retorna para a variável
+
+p1.metodo();
