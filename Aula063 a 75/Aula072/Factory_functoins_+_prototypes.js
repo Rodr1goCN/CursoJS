fa7d240a01256b2872b8b240366ae08566e1ac74
Rodrigo <123037167+Rodr1goCN@users.noreply.github.com>
2023-06-13 19:42:57 -0300

    factory functions e prototypes

--- Aula063 a 75/Aula072/Factory_functoins_+_prototypes.js
+++ Aula063 a 75/Aula072/Factory_functoins_+_prototypes.js
@@ -1,17 +1,34 @@
-function criaPessoa(nome, sobrenome){
-    const pessoaPrototype = {
-        falar(){
-            console.log(`${this.nome} esta falando`);
-        },
-        beber(){
-            console.log(`${this.nome} esta bebendo`);
-        },
-        comer(){
-            console.log(`${this.nome} esta comendo`);
-        },
+//Factory Functions = Função Fábrica
+const falar = {
+    falar() {
+        console.log(`${this.nome} esta falando`);
     }
-    return Object.create(pessoaPrototype, {
-        nome: {value: nome},
-        sobrenome: {value: sobrenome},
-    });
 }
+
+const beber = {
+    beber() {
+        console.log(`${this.nome} esta bebendo`);
+    },
+}
+
+const comer = {
+    comer() {
+        console.log(`${this.nome} esta comendo`);
+    },
+}
+
+//const pessoaPrototype = { ...falar, ...comer, ...beber };
+const pessoaPrototype = Object.assign({}, falar, comer, beber); //também podemos fazer deste jeito que funciona
+
+function criaPessoa(nome, sobrenome) {
+    return Object.create(pessoaPrototype, {
+        nome: { value: nome },
+        sobrenome: { value: sobrenome },
+    });
+
+}
+
+
+const p1 = criaPessoa('Rodrigo', 'Costa')
+const p2 = criaPessoa('Maria', 'C.')
+console.log(p1)
