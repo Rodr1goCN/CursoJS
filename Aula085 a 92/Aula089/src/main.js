c90745ec0b93c8ffa939e0e1d8eb75a691ed3aef
Rodrigo <123037167+Rodr1goCN@users.noreply.github.com>
2023-06-26 20:39:30 -0300

    gerador de CPF

--- Aula085 a 92/Aula089/src/main.js
+++
@@ -1,9 +0,0 @@
-import GeraCPF from './Modules/GeraCPF';
-
-import './assets/css/style.css';
-
-(function () {
-    const gera = new GeraCPF();
-    const cpfGerado = document.querySelector('.resultado');
-    cpfGerado.innerHTML = gera.geraNovoCPF();
-})();
