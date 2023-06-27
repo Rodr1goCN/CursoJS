4f89d370810086a6082ddf724d37e1a61093891c
Rodrigo <123037167+Rodr1goCN@users.noreply.github.com>
2023-06-27 20:37:52 -0300

    Gerador de senhas

--- Aula085 a 92/Aula090/src/Modules/geradores.js
+++ Aula085 a 92/Aula090/src/Modules/geradores.js
@@ -26,18 +26,19 @@ const geraSimbolo = () => String.fromCharCode(rand(33, 48));
 const senha = geraSenha(8)
 console.log(senha) */
 
-export default function geraSenha(qtd, maiusculas, minusculas, numeros, simbolos){
+export default function geraSenha(qtd, maiusculas, minusculas, numeros, simbolos) {
     const senhaArray = []
-    
+
     qtd = Number(qtd);
 
-    for (let i = 0; i < qtd; i++){
+    for (let i = 0; i < qtd; i++) {
         maiusculas && senhaArray.push(geraMaiuscula()); // se maiusculas for verdadeiro, executa o senhaArray, se não, não executa
         minusculas && senhaArray.push(geraMinuscula());
         numeros && senhaArray.push(geraNumero());
         simbolos && senhaArray.push(geraSimbolo());
     }
     senhaArray.sort(() => 0.5 - Math.random()) //embaralha a senha gerada // O valor 0.5 é frequentemente utilizado porque proporciona um bom equilíbrio entre aleatoriedade e efetividade no embaralhamento.
-
-    return senhaArray.join('').slice(0, qtd); //o .join('') deixa no prompt sem formatação(, ou '')
+    return senhaArray.join('').slice(0, qtd).match(/.{1,10}/g).join('<br>'); 
+    //o .join('') junta todos os elementos do senhaArray em uma string 
+    //match(/.{1,10}/g) para dividir a senha em grupos de 10 caracteres
 }
