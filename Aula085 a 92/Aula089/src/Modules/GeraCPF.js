31d33c407166f285bc84228f6856fa4fbaa46720
Rodrigo <123037167+Rodr1goCN@users.noreply.github.com>
2023-06-26 22:32:08 -0300

    gerador de CPF

--- Aula085 a 92/Aula089/src/Modules/GeraCPF.js
+++ Aula085 a 92/Aula089/src/Modules/GeraCPF.js
@@ -5,11 +5,20 @@ export default class GeraCPF {
         return String(Math.floor(Math.random() * (max - min) + min));
     }
 
+    formatando(cpf){
+        return(
+            cpf.slice(0, 3) + '.' +
+            cpf.slice(3, 6) + '.' +
+            cpf.slice(6, 9) + '-' +
+            cpf.slice(9, 11) 
+        )
+    }
+
     geraNovoCPF(){
         const cpfSemDigito = this.rand();
         const digito1 = ValidaCPF.criaDigito(cpfSemDigito);
         const digito2 = ValidaCPF.criaDigito(cpfSemDigito + digito1);
         const novoCpf = cpfSemDigito + digito1 + digito2;
-        return novoCpf;
+        return this.formatando(novoCpf);
     }
 }
