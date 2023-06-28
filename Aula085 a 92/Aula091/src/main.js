9f0934cc21dbc35582d3d62a76411a76977e64b5
Rodrigo <123037167+Rodr1goCN@users.noreply.github.com>
2023-06-28 18:11:19 -0300

    promise em computadores antigos

---
+++ Aula085 a 92/Aula091/src/main.js
@@ -0,0 +1,8 @@
+import 'core-js/stable' //para funcionar em computadores mais antigos IE 9 adiante
+import 'regenerator-runtime/runtime'//para funcionar em computadores mais antigos IE 9 adiante
+
+import executa from './Modules/promises'
+
+import './assets/css/style.css';
+
+executa();
