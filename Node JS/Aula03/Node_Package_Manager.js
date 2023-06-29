a75d8e48cabd0c77f47b577abcf77f57fc71f02e
Rodrigo <123037167+Rodr1goCN@users.noreply.github.com>
2023-06-29 19:01:45 -0300

    NPM - Node Package Modules

---
+++ Node JS/Aula03/Node_Package_Manager.js
@@ -0,0 +1,16 @@
+/* 
+// Se não quiser que os pacotes sejam atualizados para uma versão mais nova, só por o (npm install express -E) (última letra maiúscula)
+// Para atualizar (npm update)
+// mover o express para devDependencies (npm install express --save-dev)
+// voltar para dependencies (npm install express --save-prod)
+// Escolher uma versão específica (npm install express@2.1.0)
+//  2.    1.      0
+// major  minor   patch
+// Patch - (2.1.1)Correção de bugs // Minor - (2.2.0) adição de um novo recurso // Major - (3.0.0) atualização de uma nova estrutura
+// Quando se usa o ^ dentro do package.json pode atualizar o Minor e Patch
+// Para desinstalar um pacote se usa o (npm uninstall express)
+// Para verificar se tem alguma atualização pra fazer (npm outdated)
+// Mostrar pastas dentro do webpack (npm ls) e (npm ls --depth=1)
+//
+//
+*/
