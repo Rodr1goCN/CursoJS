651bb14399c77b185c8701a6e617a72334a90cf9
Rodrigo <123037167+Rodr1goCN@users.noreply.github.com>
2023-06-30 19:34:54 -0300

    express / abrir servidor node js / post / get

---
+++ Node JS/Aula06/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+//        CRIAR     LER     ATUALIZAR   APAGAR
+//CRUD -> CREATE,   READ,    UPDATE,    DELETE
+//        POST      GET      PUT        DELETE
+// http://meusite.com/ <- GET -> Entregue a página
+
+app.get('/', (requesicao, resposta) => {
+    resposta.send(`
+    <form action="/" method="POST">
+    Nome: <input type="text" name="nome">
+    <button> Enviar </button>
+    </form>
+    `);
+});
+
+app.post('/', (requesicao, resposta) => {
+    resposta.send('Recebi o formulário')
+})
+
+app.get('/contato', (requesicao, resposta) => {
+    resposta.send('Segue meu número 021 99989-9999')
+})
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor esta sendo executado na porta 3000');
+});
