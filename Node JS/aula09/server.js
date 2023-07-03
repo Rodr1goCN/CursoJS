bceb2e9e7654ef2af4d304926897fd5d3474eb31
Rodrigo <123037167+Rodr1goCN@users.noreply.github.com>
2023-07-03 19:55:38 -0300

    express view

---
+++ Node JS/aula09/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const path = require('path');
+
+
+app.use(express.urlencoded({extended:true}));//faz ver o que foi escrito dentro do formulário
+app.set('views', path.resolve(__dirname, 'src', 'views'));//funciona também desse jeito './src/views'
+app.set('view engine', 'ejs');
+app.use(routes);
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor esta sendo executado na porta 3000');
+});
