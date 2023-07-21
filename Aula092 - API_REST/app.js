3e65801959c13a1bfbb6e16045008760bbfc1ec8
Rodrigo Costa <drigocn2@hotmail.com>
2023-07-21 19:14:21 -0300

    Aula092 - API REST

---
+++ Aula092 - API_REST/app.js
@@ -0,0 +1,7 @@
+const teste = 22;
+// se não tivesse uma regra no .eslintrc.js dava para usar o comando abaixo.
+console.log(teste); //eslint-disable-line
+
+export default () => {
+  console.log('ola mundão de meu Deus!');
+};
