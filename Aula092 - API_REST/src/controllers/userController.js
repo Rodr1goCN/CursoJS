53c01cec312c416ce216100799afb19410e3e278
Rodrigo Costa <drigocn2@hotmail.com>
2023-07-25 21:17:54 -0300

    Aula092 - API_REST

--- Aula092 - API_REST/src/controllers/userController.js
+++ Aula092 - API_REST/src/controllers/userController.js
@@ -9,23 +9,83 @@ class UserController {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
-      console.log(e);
+    }
+  }
+
+  // O QUE FALTA FAZER
+  // INDEX
+  async index(req, res) {
+    try {
+      const usuarios = await User.findAll();
+      return res.json(usuarios);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  // SHOW
+  async show(req, res) {
+    try {
+      const usuario = await User.findByPk(req.params.id); // findByPk = encontrar pela primary key
+      return res.json(usuario);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  // UPDATE
+  async update(req, res) {
+    try {
+      if (req.param.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado.'],
+        });
+      }
+
+      const usuario = await User.findByPk(req.params.id); // findByPk = encontrar pela primary key
+
+      if (!usuario) {
+        return res.status(400).json({
+          errors: ['Usuário não existe.'],
+        });
+      }
+
+      const novosDados = await usuario.update(req.body);
+
+      return res.json(novosDados);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // DELETE
+  async delete(req, res) {
+    try {
+      if (req.param.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado.'],
+        });
+      }
+
+      const usuario = await User.findByPk(req.params.id); // findByPk = encontrar pela primary key
+
+      if (!usuario) {
+        return res.status(400).json({
+          errors: ['Usuário não existe.'],
+        });
+      }
+
+      await usuario.destroy();
+
+      return res.json(null);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
     }
   }
 }
-// O QUE FALTA FAZER
-// INDEX
-async index(req, res){
-  try {
-    const usuarios = await User.findAll();
-    return res.json(usuarios)
-  }catch (e){
-    return res.json(null);
-  }
-}
-// SHOW
 
-// UPDATE
-
-// DELETE
 export default new UserController();
