50d4ec9c26663eb89d3766140d3871b1fe374d82
Rodrigo Costa <drigocn2@hotmail.com>
2023-07-28 21:12:54 -0300

    Aula092 - API_REST
    criado alunocontroller.js e alunoRoutes.js e editado alguns arquivos

--- Aula092 - API_REST/src/controllers/HomeController.js
+++ Aula092 - API_REST/src/controllers/HomeController.js
@@ -2,15 +2,7 @@ import Aluno from '../models/Aluno';
 
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Maria',
-      sobrenome: 'Silva',
-      email: 'mariasilva@hotmail.com',
-      idade: 23,
-      peso: 50,
-      altura: 1.65,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
