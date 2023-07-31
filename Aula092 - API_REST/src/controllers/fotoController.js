227de592598dee546d8c689deebf859c03f4cec0
Rodrigo Costa <drigocn2@hotmail.com>
2023-07-31 20:10:39 -0300

    Aula092 - API_REST

---
+++ Aula092 - API_REST/src/controllers/fotoController.js
@@ -0,0 +1,10 @@
+import multer from 'multer';
+import multerConfig from '../config/multerConfig';
+
+class FotoController {
+  async store(req, res) {
+    res.json(req.file);
+  }
+}
+
+export default new FotoController();
