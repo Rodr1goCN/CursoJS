c0a9dc763151527c6ad3827a12f830a803baf82d
Rodrigo Costa <drigocn2@hotmail.com>
2023-08-11 19:34:18 -0300

    Aula093 - React
    funcionalidades editar e deletar

--- Aula093 - React/aula01-projeto/src/components/Main.js
+++ Aula093 - React/aula01-projeto/src/components/Main.js
@@ -11,24 +11,55 @@ export default class Main extends Component {
   state = {
     novaTarefa: "",
     tarefas: [],
+    index: -1,
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const { tarefas } = this.state;
+    const { tarefas, index } = this.state;
     let { novaTarefa } = this.state;
     novaTarefa = novaTarefa.trim(); // Elimina os espaços vazios da frase
 
     if (tarefas.indexOf(novaTarefa) !== -1) return;
     const novasTarefas = [...tarefas];
-    this.setState({
-      tarefas: [...novasTarefas, novaTarefa],
-    });
+
+    if (index === -1) {
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+        novaTarefa: "",
+      });
+    } else {
+      novasTarefas[index] = novaTarefa;
+
+      this.setState({
+        tarefas: [...novasTarefas],
+        index: -1,
+        novaTarefa: "",
+      });
+    }
   };
 
   handleChange = (e) => {
     this.setState({
       novaTarefa: e.target.value,
+    });
+  };
+
+  handleEdit = (e, index) => {
+    const { tarefas } = this.state;
+    this.setState({
+      index,
+      novaTarefa: tarefas[index],
+    });
+  };
+
+  handleDelete = (e, index) => {
+    const { tarefas } = this.state; // Usa a desestruturação para obter o array de tarefas do estado do componente. Isso pressupõe que o componente tem um estado (this.state) que contém um array chamado tarefas.
+    const novasTarefas = [...tarefas]; // Cria uma cópia do array de tarefas usando o operador de spread ([...tarefas]). Isso é feito para evitar a modificação do estado original, o que é uma boa prática de programação.
+    novasTarefas.splice(index, 1); // Usa o método splice para remover 1 elemento do array novasTarefas na posição index. Isso tem o efeito de excluir a tarefa na posição indicada.
+    this.setState({
+      // Atualiza o estado do componente usando setState(). Ele define o estado tarefas com a nova lista de tarefas, que agora não inclui mais a tarefa removida. Isso aciona uma re-renderização do componente, refletindo as mudanças na interface do usuário.
+      tarefas: [...novasTarefas],
     });
   };
 
@@ -48,12 +79,18 @@ export default class Main extends Component {
           </button>
         </form>
         <ul className="tarefas">
-          {tarefas.map((tarefa) => (
+          {tarefas.map((tarefa, index) => (
             <li key={tarefa}>
               {tarefa}
               <div>
-                <FaEdit className="edit" />
-                <FaWindowClose className="delete" />
+                <FaEdit
+                  onClick={(e) => this.handleEdit(e, index)}
+                  className="edit"
+                />
+                <FaWindowClose
+                  onClick={(e) => this.handleDelete(e, index)}
+                  className="delete"
+                />
               </div>
             </li>
           ))}
