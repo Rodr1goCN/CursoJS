b16a8b234b24626aa1cceb2a6dd97189065e5995
Rodrigo Costa <drigocn2@hotmail.com>
2023-09-05 20:10:01 -0300

    Aula093 - React\aula02
    Adicionando login #2

--- Aula093 - React/aula02-consumirAPI/src/routes/MyRoutes.js
+++ Aula093 - React/aula02-consumirAPI/src/routes/MyRoutes.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
+import { useSelector } from 'react-redux';
 
 // eslint-disable-next-line react/prop-types
 export default function MyRoute({ component: Component, isClosed, ...rest }) {
-  const isLoggedIn = false;
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
 
   if (isClosed && !isLoggedIn) {
     return (
       <Redirect
-        to={{ pathname: '/', state: { prevPath: rest.location.pathname } }}
+        to={{ pathname: '/login', state: { prevPath: rest.location.pathname } }}
       />
     );
   }
