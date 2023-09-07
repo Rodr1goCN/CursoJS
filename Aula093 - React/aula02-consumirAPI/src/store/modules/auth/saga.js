1c72fafdf35e670debedc092d441ac14c233bb5b
Rodrigo Costa <drigocn2@hotmail.com>
2023-09-07 20:22:42 -0300

    Aula093 - React\aula02
    Editando usuário loggado

--- Aula093 - React/aula02-consumirAPI/src/store/modules/auth/saga.js
+++ Aula093 - React/aula02-consumirAPI/src/store/modules/auth/saga.js
@@ -28,7 +28,15 @@ function persistRehydrate({ payload }){
     if(!token) return;
     axios.defaults.headers.Authorization = `Bearer ${token}`;
 }
+
+function registerRequest({ payload }){
+const {id, nome, email, password} = payload;
+console.log('continua no próximo capítulo');
+
+}
+
 export default all([
     takeLatest(types.LOGIN_REQUEST, loginRequest), 
-    takeLatest(types.PERSIST_REHYDRATE, persistRehydrate)
+    takeLatest(types.PERSIST_REHYDRATE, persistRehydrate),
+    takeLatest(types.REGISTER_REQUEST, registerRequest)
 ]);
