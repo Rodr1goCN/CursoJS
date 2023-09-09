891de9835bd75d93196736f6eac00a52277e2308
Rodrigo Costa <drigocn2@hotmail.com>
2023-09-08 22:40:14 -0300

    Aula093 - React\aula02
    Edição de usuário logado

--- Aula093 - React/aula02-consumirAPI/src/store/modules/auth/saga.js
+++ Aula093 - React/aula02-consumirAPI/src/store/modules/auth/saga.js
@@ -29,10 +29,47 @@ function persistRehydrate({ payload }){
     axios.defaults.headers.Authorization = `Bearer ${token}`;
 }
 
-function registerRequest({ payload }){
+function* registerRequest({ payload }){
 const {id, nome, email, password} = payload;
-console.log('continua no próximo capítulo');
 
+try {
+    if(id){
+        yield call(axios.put, '/users',{
+            email,
+            nome,
+            password: password || undefined,
+        })
+        toast.success('Editado com sucesso');
+        yield put(actions.registerUpdatedSuccess({ nome, email, password }));
+    } else {
+        yield call(axios.post, '/users',{
+            email,
+            nome,
+            password: password || undefined,
+        })
+        toast.success('Conta criada com sucesso');
+        yield put(actions.registerCreatedSuccess({ nome, email, password }));
+        history.push('/login');
+
+    }
+} catch (err) {
+    const errors = get(err, 'response.data.error', []);
+    const status = get(err, 'response.status', 0);
+
+    if(status === 401){
+        toast.error('Você precisa fazer login novamente');
+        yield put(actions.loginFailure);
+        return history.push('/login');
+    }
+
+    if(errors.length > 0){
+        errors.map(error => toast.error(error));
+    }else{
+        toast.error('Erro desconhecido');
+  }
+  yield put(actions.registerFailure());
+ }
+ return 1;
 }
 
 export default all([
