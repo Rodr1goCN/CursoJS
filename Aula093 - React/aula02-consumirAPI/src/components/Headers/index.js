f549a70e9a145ca77d65d0eac489339594d81e35
Rodrigo Costa <drigocn2@hotmail.com>
2023-09-11 19:35:38 -0300

    logout e excluir alunos

--- Aula093 - React/aula02-consumirAPI/src/components/Headers/index.js
+++ Aula093 - React/aula02-consumirAPI/src/components/Headers/index.js
@@ -1,20 +1,44 @@
 import React from "react";
-import { FaHome, FaSignInAlt, FaUserAlt } from "react-icons/fa";
+import { FaHome, FaSignInAlt, FaUserAlt, FaCircle, FaPowerOff } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import * as actions from '../../store/modules/auth/action'
+import history from "../../services/history";
+
 import { Nav } from "./styled";
 
 export default function Header() {
-  return (
-    <Nav>
-      <Link to="/">
-        <FaHome size={25} />
-      </Link>
-      <Link to="/register">
-        <FaUserAlt size={25} />
-      </Link>
-      <Link to="/login">
-        <FaSignInAlt size={25} />
-      </Link>
-    </Nav>
-  );
+    const dispatch = useDispatch();
+    const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+    const handLogout = e => {
+        e.preventDefault();
+
+        dispatch(actions.loginFailure());
+        history.push('/')
+    };
+
+    return (
+        <Nav>
+            <Link to="/">
+                <FaHome size={25} />
+            </Link>
+            <Link to="/register">
+                <FaUserAlt size={25} />
+            </Link>
+
+            {isLoggedIn ? (
+                <Link onClick={handLogout} to="/logout">
+                    <FaPowerOff size={25} />
+                </Link>
+            ) : (
+                <Link to="/login">
+                    <FaSignInAlt size={25} />
+                </Link>
+            )}
+
+
+            {isLoggedIn && <FaCircle size={25} color="#19FA56" />}
+
+        </Nav>
+    );
 }
--- Aula093 - React/aula02-consumirAPI/src/pages/Alunos/index.js
+++ Aula093 - React/aula02-consumirAPI/src/pages/Alunos/index.js
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from "react";
 import { Link } from 'react-router-dom'
 import { get } from 'lodash';
-import { FaUserCircle, FaEdit, FaWindowClose } from 'react-icons/fa'
+import { FaUserCircle, FaEdit, FaWindowClose, FaExclamation } from 'react-icons/fa'
+import { toast} from 'react-toastify';
 
 import { Container } from "../../styles/GlobalStyles";
 import { AlunoContainer, ProfilePicture } from "./styled";
@@ -23,14 +24,44 @@ export default function Alunos() {
     }
 
     getData();
-  }, [])
+  }, []);
+
+  const hanbleDeleteAsk = e => {
+    e.preventDefault();
+    const exclamation = e.currentTarget.nextSibling;
+    exclamation.setAttribute('display', 'block');
+    e.currentTarget.remove();
+  };
+
+  const handleDelete = async(e, id, index) => {
+    e.persist()
+    try {
+        setIsLoading(true);
+        await axios.delete(`/alunos/${id}`)
+        const novosAlunos = [...alunos];
+        novosAlunos.splice(index, 1);
+        setAlunos(novosAlunos);
+        setIsLoading(false);
+    } catch (err) {
+        const status = get(err, 'response.status', 0);
+
+        if(status === 401){
+          toast.error('Você precisa fazer o login novamente');
+          
+        }else{
+          toast.error('Ocorreu um erro ao excluir aluno');
+        }
+        setIsLoading(false);
+    }
+  }
+
   return (
     <Container>
         <Loading isLoading={isLoading} />
       <h1>Alunos</h1>
 
       <AlunoContainer>
-        {alunos.map(aluno => (
+        {alunos.map((aluno, index) => (
           <div key={String(aluno.id)}>
             <ProfilePicture>
           {get(aluno, 'Fotos[0].url', false) ? (<img src={aluno.Fotos[0].url} alt="" />) : (<FaUserCircle size={30} />)}
@@ -40,7 +71,9 @@ export default function Alunos() {
 
           <Link to={`/aluno/${aluno.id}/edit`}><FaEdit size={15} /></Link>
 
-          <Link to={`/aluno/${aluno.id}/delete`}><FaWindowClose size={15} /></Link>
+          <Link onClick={hanbleDeleteAsk} to={`/aluno/${aluno.id}/delete`}><FaWindowClose size={15} /></Link>
+          <FaExclamation 
+          size={15} display='none' cursor='pointer' onClick={e => handleDelete(e, aluno.id, index)}/>
           </div>
           
         ))}
