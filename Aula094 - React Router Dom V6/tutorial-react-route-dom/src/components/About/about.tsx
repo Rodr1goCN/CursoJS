b1575c5de89b04b3185f3784cc9525d8df04e380
Rodrigo Costa <drigocn2@hotmail.com>
2023-09-18 21:42:56 -0300

    Aula094 - React Router Dom
    Link vs a

---
+++ Aula094 - React Router Dom V6/tutorial-react-route-dom/src/components/About/about.tsx
@@ -0,0 +1,9 @@
+import './style.css';
+
+export const About = () => {
+    return (
+        <div>
+            <h1>Sobre o site</h1>
+        </div>
+    )
+}
--- Aula094 - React Router Dom V6/tutorial-react-route-dom/src/main.tsx
+++ Aula094 - React Router Dom V6/tutorial-react-route-dom/src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Home } from './components/Home';
+import { About } from './components/About/about';
 import './Styles/Global.css';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Menu } from './components/Menu';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
