9b4bf2036259eaff48df78b10bb6121b99713ff1
Rodrigo Costa <drigocn2@hotmail.com>
2023-09-20 21:55:30 -0300

    Aula095 - Expressões_Regulares
    o uso do g, i, () e |

---
+++ Aula095 - EXP_REG/aula001.js
@@ -0,0 +1,27 @@
+// g - global (encontra todas as ocorrências)
+// i - insensitive (não tem diferença entre letras (pode ser maiúscula ou minúscula))
+// () - Podemos separar por grupos
+// | - Ou
+
+const { texto } = require('./base_aula1');
+
+const regExp1 = /(Pedro, |Maria, |joão, )(sempre o admirava por sua generosidade e empatia)/i;
+const encontrado = regExp1.exec(texto)
+
+// Faz a busca para ver se há a palavra João ou joão no texto.
+console.log(regExp1.test(texto));
+console.log();
+// Podemos usar o (texto)[0] - verifica se existe a palavra joão ou João e a retorna
+// Podemos usar o (texto).index -  mostra em qual index a palavra desejada esta 
+console.log(encontrado.index);
+console.log();
+ console.log(encontrado[0]);
+console.log(encontrado[1]);
+console.log(encontrado[2]);
+
+// Podemos adicionar um if (encontrado){} para caso a palavra não existir no texto e não mostrar o erro de null
+if (encontrado) {
+    console.log(encontrado[0]);
+    console.log(encontrado[1]);
+    console.log(encontrado[2]);
+}
---
+++ Aula095 - EXP_REG/base_aula1.js
@@ -0,0 +1,9 @@
+const texto = `João era um homem de princípios sólidos, cuja bondade era conhecida por todos na pequena cidade de Santa Clara. Ele era filho de Maria e José, nomes que também eram respeitados na comunidade. Desde jovem, João demonstrava uma paixão por ajudar os outros.
+
+Seu amigo mais próximo, Pedro, sempre o admirava por sua generosidade e empatia. João também tinha uma irmã, Ana, que compartilhava seu espírito altruísta. Eles frequentemente se uniam para realizar obras de caridade na cidade.
+
+João conheceu Maria, uma mulher igualmente compassiva, e os dois se apaixonaram profundamente. Juntos, construíram uma família sólida e criaram seus filhos com os mesmos valores que os guiaram ao longo da vida.
+
+Hoje em dia, a memória de João perdura na cidade, e seu nome é frequentemente associado à compaixão e solidariedade. Sua influência positiva continua a inspirar as gerações futuras, deixando um legado de bondade que jamais será esquecido.`
+
+module.exports = { texto }
