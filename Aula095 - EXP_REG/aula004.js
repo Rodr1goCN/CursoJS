dc11d3f75a566c9cb9c9f658f3daee3ef9c8d15e
Rodrigo Costa <drigocn2@hotmail.com>
2023-09-22 20:06:48 -0300

    Aula095 - EXP_REG
    Removendo algumas coisas da aula004

--- Aula095 - EXP_REG/aula004.js
+++ Aula095 - EXP_REG/aula004.js
@@ -1,4 +1,4 @@
-const { texto, arquivos, html } = require('./base_aula');
+const { html } = require('./base_aula');
 
 console.log(html)
 console.log(html.match(/<.+>.+<\/.+>/g)); // .+ Qualquer caracter exceto uma quebra de linha (GREEDY)
