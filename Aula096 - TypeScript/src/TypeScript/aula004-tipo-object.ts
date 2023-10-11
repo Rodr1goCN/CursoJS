b3c8baf2528e82c0a0a1a26ac1914b9a7d9e8acf
Rodrigo Costa <drigocn2@hotmail.com>
2023-10-11 19:23:32 -0300

    Aula096 - TypeScript
    update da aula004 - tipo objects

--- Aula096 - TypeScript/src/TypeScript/aula004-tipo-object.ts
+++ Aula096 - TypeScript/src/TypeScript/aula004-tipo-object.ts
@@ -2,12 +2,15 @@ const objetoA: {
   readonly chaveA: string; // trava o valor da chave, não podendo muda-la
   chaveB: string;
   chaveC: string; // Temos que definir um tipo para a chave para mudar o valor
+  chaveD: string;
   [key: string]: unknown; // Também funciona
 } = {
   chaveA: 'valor A',
   chaveB: 'valor B',
+  chaveC: 'Valor C',
+  chaveD: 'valor D',
 };
 
-objetoA.chaveA = 'Outro valor';
+// objetoA.chaveA = 'Outro valor'; // Não podemos mudar o valor de uma chave quando esta definida como -> readonly
 objetoA.chaveC = 'Nova chave'; // Não conseguimos mudar o valor de uma chave se ela não existir
 objetoA.chaveD = 'Nova chave'; // Não conseguimos mudar o valor de uma chave se ela não existir
