c04426db89e1a865f050d6f061cd064d3cedc12a
Rodrigo Costa <drigocn2@hotmail.com>
2023-10-11 19:17:58 -0300

    Aula096 - TypeScript
    update da aula002 - tipo any

--- Aula096 - TypeScript/src/TypeScript/aula002-tipo-any.ts
+++ Aula096 - TypeScript/src/TypeScript/aula002-tipo-any.ts
@@ -1,9 +1,9 @@
 // utilize any apenas em último caso
-function showMessage(msg /* string */) {
+function showMessage(msg: string) {
   // Se não definir algo com o 'msg' (tipo: number, boolean, string e etc) irá ficar any (qualquer coisa)
   return msg;
 }
 
-console.log(showMessage([1, 2, 3])); // Não aceitou pos o msg esta definido como string
+//console.log(showMessage([1, 2, 3])); // Não aceitou pos o msg esta definido como string
 console.log(showMessage('olá'));
-console.log(showMessage([1])); // Não aceitou pos o msg esta definido como string
+//console.log(showMessage([1])); // Não aceitou pos o msg esta definido como string
