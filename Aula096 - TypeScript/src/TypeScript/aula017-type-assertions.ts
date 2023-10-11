3814203e8d7516af0e21259bbaa6d24d03f6710a
Rodrigo Costa <drigocn2@hotmail.com>
2023-10-11 19:14:08 -0300

    Aula096 - TypeScript
    type assertions

---
+++ Aula096 - TypeScript/src/TypeScript/aula017-type-assertions.ts
@@ -0,0 +1,21 @@
+/* Recomendado */
+// Condicional
+const body = document.querySelector('body');
+if (body) body.style.background = 'red'; // Se o body for nulo, aparece uma msg de erro e se realmente existir, faz uma checagem colocando o -> if(body) no começo
+
+// Type assestion
+const body3 = document.querySelector('body') as HTMLBodyElement;
+body3.style.background = 'red';
+
+// HTMLElement
+const input = document.querySelector('.input') as HTMLInputElement;
+input.value = 'Qualquer coisa';
+
+/* Não recomendado */
+// Type assestion
+const body4 = document.querySelector('body') as unknown as number; // Se precisar que o body seja um number, primeiro tem que colocar: as unknown
+body4.style.background = 'red';
+
+// Non-null assertion (!)
+const body2 = document.querySelector('body')!; // Usando o ponto de interrogação informa que o body não pode ser nulo
+body2.style.background = 'red';
