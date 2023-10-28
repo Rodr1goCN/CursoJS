b45dd95d5e54eb4685f6fd7b02b38482d09c4407
Rodrigo Costa <drigocn2@hotmail.com>
2023-10-27 23:48:50 -0300

    Aula096 - TypeScript
    aula 035 - adicionado botão mute e fullscreen

--- Aula096 - TypeScript/src/TypeScript/Aula035-exercício-vídeo/aula035-exercício-vídeo.ts
+++ Aula096 - TypeScript/src/TypeScript/Aula035-exercício-vídeo/aula035-exercício-vídeo.ts
@@ -2,6 +2,8 @@ interface VideoPlayerElements {
   videoPlayer: HTMLVideoElement;
   playButton: HTMLButtonElement;
   stopButton: HTMLButtonElement;
+  muteButton: HTMLButtonElement;
+  fullscreenButton: HTMLButtonElement;
 }
 
 interface VideoPlayerProtocol {
@@ -14,11 +16,15 @@ export default class VideoPlayer implements VideoPlayerProtocol {
   private videoPlayer: HTMLVideoElement;
   private playButton: HTMLButtonElement;
   private stopButton: HTMLButtonElement;
+  private muteButton: HTMLButtonElement;
+  private fullscreenButton: HTMLButtonElement;
 
   constructor(videoPlayerElements: VideoPlayerElements) {
     this.videoPlayer = videoPlayerElements.videoPlayer;
     this.playButton = videoPlayerElements.playButton;
     this.stopButton = videoPlayerElements.stopButton;
+    this.muteButton = videoPlayerElements.muteButton;
+    this.fullscreenButton = videoPlayerElements.fullscreenButton;
   }
 
   iniciarEventos(): void {
@@ -31,7 +37,30 @@ export default class VideoPlayer implements VideoPlayerProtocol {
       this.videoPlayer.currentTime = 0;
       this.playButton.innerText = 'Play';
     });
+
+    this.muteButton.addEventListener('click', () => {
+      this.toggleMute();
+    });
+    this.fullscreenButton.addEventListener('click', () => {
+      this.toggleFullscreen();
+    });
   }
+
+  toggleFullscreen(): void {
+    if (this.videoPlayer.requestFullscreen) {
+      this.videoPlayer.requestFullscreen();
+    }
+  }
+
+  toggleMute(): void {
+    this.videoPlayer.muted = !this.videoPlayer.muted;
+    if (this.videoPlayer.muted) {
+      this.muteButton.innerText = 'Unmute';
+    } else {
+      this.muteButton.innerHTML = 'Mute';
+    }
+  }
+
   playToggle(): void {
     if (this.videoPlayer.paused) {
       this.videoPlayer.play();
@@ -47,6 +76,8 @@ const videoPlayer = new VideoPlayer({
   videoPlayer: document.querySelector('.video') as HTMLVideoElement,
   playButton: document.querySelector('.play') as HTMLButtonElement,
   stopButton: document.querySelector('.stop') as HTMLButtonElement,
+  muteButton: document.querySelector('.mute') as HTMLButtonElement,
+  fullscreenButton: document.querySelector('.fullscreen') as HTMLButtonElement,
 });
 
 videoPlayer.iniciarEventos();
