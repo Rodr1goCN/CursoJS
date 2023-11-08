20c6b6cc329cb7505522cc0893679a1ce8c079fd
Rodrigo Costa <drigocn2@hotmail.com>
2023-11-08 20:01:57 -0300

    Aula096 - TypeScript
    aula 56 - docorator method

---
+++ Aula096 - TypeScript/src/TypeScript/Aula056-decorator-method/aula056-decorator-method.ts
@@ -0,0 +1,46 @@
+function decorador(
+  classProtype: any,
+  nomeMetodo: string,
+  descriptor: PropertyDescriptor,
+): PropertyDescriptor | void {
+  console.log(classProtype);
+  console.log(nomeMetodo);
+  console.log(descriptor);
+  /* return {
+    value: function (...args: string[]) {
+      return args[0].toUpperCase();
+    },
+  }; */
+}
+
+export class UmaPessoa {
+  nome: string;
+  sobrenome: string;
+  idade: number;
+
+  constructor(nome: string, sobrenome: string, idade: number) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+  }
+  @decorador
+  metodo(msg: string): string {
+    return `${this.nome} ${this.sobrenome}: ${msg}`;
+  }
+
+  get nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+
+  set nomeCompleto(valor: string) {
+    const palavras = valor.split(/\s+/g);
+    const primeiroNome = palavras.shift();
+    if (!primeiroNome) return;
+    this.nome = primeiroNome;
+    this.sobrenome = palavras.join(' ');
+  }
+}
+
+const pessoa = new UmaPessoa('Rodrigo', 'Costa', 38);
+const metodo = pessoa.metodo('OPA, sou uma mensagem');
+console.log(metodo);
