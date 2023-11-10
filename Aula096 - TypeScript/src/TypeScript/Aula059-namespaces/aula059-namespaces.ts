4e9555cc015c320fe13d126f0e3b38d1371abc23
Rodrigo Costa <drigocn2@hotmail.com>
2023-11-10 19:17:48 -0300

    Aula096 - TypeScript
    aula 59 - namespace

---
+++ Aula096 - TypeScript/src/TypeScript/Aula059-namespaces/aula059-namespaces.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+namespace MeuNamespace {
+  export const nomeDoNamespace = 'Rodrigo';
+  // export para usar o PessoaDoNamespace fora do escopo
+  export class PessoaDoNamespace {
+    constructor(public nome: string) {}
+  }
+  const pessoa = new PessoaDoNamespace('Rodrigo');
+  console.log(pessoa);
+
+  export namespace OutroNamespace {
+    export const nomeDoNamespace = 'Nome do outro namespace';
+  }
+}
+
+const pessoaDoNamespace = new MeuNamespace.PessoaDoNamespace('Rodrigo');
+console.log(pessoaDoNamespace);
+
+console.log(MeuNamespace.OutroNamespace.nomeDoNamespace);
+
+console.log(MeuNamespace.nomeDoNamespace);
+
+export default 1;
