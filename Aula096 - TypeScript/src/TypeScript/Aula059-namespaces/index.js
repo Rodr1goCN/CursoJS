63c14c3aba7be8c5f94475515b040df9189e5991
Rodrigo Costa <drigocn2@hotmail.com>
2023-11-13 19:18:56 -0300

    Aula096 - TypeScript
    Removendo alguns espaços

--- Aula096 - TypeScript/src/TypeScript/Aula059-namespaces/index.js
+++ Aula096 - TypeScript/src/TypeScript/Aula059-namespaces/index.js
@@ -1,21 +1,21 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 var MeuNamespace;
 (function (MeuNamespace) {
-    MeuNamespace.nomeDoNamespace = 'Rodrigo';
-    // export para usar o PessoaDoNamespace fora do escopo
-    var PessoaDoNamespace = /** @class */ (function () {
-        function PessoaDoNamespace(nome) {
-            this.nome = nome;
-        }
-        return PessoaDoNamespace;
-    }());
-    MeuNamespace.PessoaDoNamespace = PessoaDoNamespace;
-    var pessoa = new PessoaDoNamespace('Rodrigo');
-    console.log(pessoa);
-    var OutroNamespace;
-    (function (OutroNamespace) {
-        OutroNamespace.nomeDoNamespace = 'Nome do outro namespace';
-    })(OutroNamespace = MeuNamespace.OutroNamespace || (MeuNamespace.OutroNamespace = {}));
+  MeuNamespace.nomeDoNamespace = 'Rodrigo';
+  // export para usar o PessoaDoNamespace fora do escopo
+  var PessoaDoNamespace = /** @class */ (function () {
+    function PessoaDoNamespace(nome) {
+      this.nome = nome;
+    }
+    return PessoaDoNamespace;
+  })();
+  MeuNamespace.PessoaDoNamespace = PessoaDoNamespace;
+  var pessoa = new PessoaDoNamespace('Rodrigo');
+  console.log(pessoa);
+  var OutroNamespace;
+  (function (OutroNamespace) {
+    OutroNamespace.nomeDoNamespace = 'Nome do outro namespace';
+  })((OutroNamespace = MeuNamespace.OutroNamespace || (MeuNamespace.OutroNamespace = {})));
 })(MeuNamespace || (MeuNamespace = {}));
 var pessoaDoNamespace = new MeuNamespace.PessoaDoNamespace('Rodrigo');
 console.log(pessoaDoNamespace);
