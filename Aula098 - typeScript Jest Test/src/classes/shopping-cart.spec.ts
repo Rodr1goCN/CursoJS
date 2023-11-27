16792f08a027110c3340037951cf62aee77caba6
Rodrigo Costa <drigocn2@hotmail.com>
2023-11-27 19:43:48 -0300

    Aula098 - TypeScript - Jest Test
    testando o shopping cart

---
+++ Aula098 - typeScript Jest Test/src/classes/shopping-cart.spec.ts
@@ -0,0 +1,77 @@
+import { ShoppingCart } from './shopping-cart';
+import { Discount } from './discount';
+import { CartItem } from './interfaces/cart-item';
+
+const createSut = () => {
+  const discountMock = createDiscountMock();
+  const sut = new ShoppingCart(discountMock);
+  return { sut, discountMock };
+};
+
+const createDiscountMock = () => {
+  class DiscountMock extends Discount {}
+  return new DiscountMock();
+};
+
+const createCartItem = (name: string, price: number, brand: string) => {
+  class CartItemMock implements CartItem {
+    constructor(
+      public name: string,
+      public price: number,
+      public brand: string,
+    ) {}
+  }
+  return new CartItemMock(name, price, brand);
+};
+
+const createSutWithProducts = () => {
+  // System Under Test
+  const { sut, discountMock } = createSut();
+  const cartITem1 = createCartItem('Camiseta', 30, 'Nike');
+  const cartITem2 = createCartItem('Caneca', 15, 'Short');
+  sut.addItem(cartITem1);
+  sut.addItem(cartITem2);
+  return { sut, discountMock };
+};
+
+describe('shoppingCart', () => {
+  it('should be an empty cart when no product is added', () => {
+    // System Under Test
+    const { sut } = createSut();
+    expect(sut.isEmpty()).toBe(true);
+  });
+
+  it('should have two cart items', () => {
+    // System Under Test
+    const { sut } = createSutWithProducts();
+    expect(sut.items.length).toBe(2);
+  });
+
+  it('should test total and totalWithDiscount', () => {
+    // System Under Test
+    const { sut } = createSutWithProducts();
+    expect(sut.total()).toBe(45);
+    expect(sut.totalWithDiscount()).toBe(45);
+  });
+
+  it('should add products and clear cart', () => {
+    // System Under Test
+    const { sut } = createSutWithProducts();
+    expect(sut.items.length).toBe(2);
+    sut.clear();
+    expect(sut.items.length).toBe(0);
+    expect(sut.isEmpty()).toBe(true);
+  });
+
+  it('should remove products', () => {
+    // System Under Test
+    const { sut } = createSutWithProducts();
+    expect(sut.items.length).toBe(2);
+    sut.removeItem(1);
+    expect(sut.items.length).toBe(1);
+    sut.removeItem(0);
+    expect(sut.isEmpty()).toBe(true);
+  });
+});
+
+// Mock - Objeto que finje que é uma dependência
