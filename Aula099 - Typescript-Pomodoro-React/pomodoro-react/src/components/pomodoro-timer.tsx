1ea1f937659a9ef211796eb5cd39e1d3d6033d6a
Rodrigo Costa <drigocn2@hotmail.com>
2023-12-01 19:15:28 -0300

    Aula099 - Typescript - Pomodoro
    Adicionado botão e o timer

--- Aula099 - Typescript-Pomodoro-React/pomodoro-react/src/components/pomodoro-timer.tsx
+++ Aula099 - Typescript-Pomodoro-React/pomodoro-react/src/components/pomodoro-timer.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useInterval } from '../hook/use-interval';
+import { Button } from './button';
+//eslint-disable-next-line
 import { secondsToTime } from '../utils/seconds-to-time';
+import { Timer } from './timer';
 
 interface Props {
   defaultPomodoroTimer: number;
@@ -13,5 +16,11 @@ export function PomodoroTimer(props: Props): JSX.Element {
     setMainTime(mainTime - 1); // mainTime sempre vai ser 1 segundo
   }, 1000);
 
-  return <div> olá mundão {secondsToTime(mainTime)}</div>;
+  return (
+    <div className="pomodoro">
+      <h2>You are: Working</h2>
+      <Timer mainTime={mainTime} />
+      <Button text="teste" onClick={() => console.log(1)}></Button>
+    </div>
+  );
 }
