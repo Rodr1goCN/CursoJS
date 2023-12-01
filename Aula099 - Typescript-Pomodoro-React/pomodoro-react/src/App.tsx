14046212964272ad64b19b9b1b055b8ce9402780
Rodrigo Costa <drigocn2@hotmail.com>
2023-12-01 20:13:32 -0300

    Aula099 - Typescript - Pomodoro
    criado o botão de iniciar e pausar

--- Aula099 - Typescript-Pomodoro-React/pomodoro-react/src/App.tsx
+++ Aula099 - Typescript-Pomodoro-React/pomodoro-react/src/App.tsx
@@ -3,8 +3,8 @@ import { PomodoroTimer } from './components/pomodoro-timer';
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <PomodoroTimer defaultPomodoroTimer={1500} />
+    <div className="container">
+      <PomodoroTimer pomodoroTimer={1500} shortRestTime={300} LongRestTime={900} cycles={4} />
     </div>
   );
 }
--- Aula099 - Typescript-Pomodoro-React/pomodoro-react/src/components/pomodoro-timer.tsx
+++ Aula099 - Typescript-Pomodoro-React/pomodoro-react/src/components/pomodoro-timer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useInterval } from '../hook/use-interval';
 import { Button } from './button';
 //eslint-disable-next-line
@@ -6,21 +6,58 @@ import { secondsToTime } from '../utils/seconds-to-time';
 import { Timer } from './timer';
 
 interface Props {
-  defaultPomodoroTimer: number;
+  pomodoroTimer: number;
+  shortRestTime: number;
+  LongRestTime: number;
+  cycles: number;
 }
 
 export function PomodoroTimer(props: Props): JSX.Element {
-  const [mainTime, setMainTime] = React.useState(props.defaultPomodoroTimer);
+  const [mainTime, setMainTime] = React.useState(props.pomodoroTimer);
+  const [timeCounting, setTimeCounting] = React.useState(false);
+  const [working, setWorking] = React.useState(false);
 
-  useInterval(() => {
-    setMainTime(mainTime - 1); // mainTime sempre vai ser 1 segundo
-  }, 1000);
+  useEffect(() => {
+    if (working) document.body.classList.add('working');
+  }, [working]);
+
+  useInterval(
+    () => {
+      setMainTime(mainTime - 1); // mainTime sempre vai ser 1 segundo
+    },
+    timeCounting ? 1000 : null,
+  );
+
+  const configureWork = () => {
+    setTimeCounting(true);
+    setWorking(true);
+  };
 
   return (
     <div className="pomodoro">
       <h2>You are: Working</h2>
       <Timer mainTime={mainTime} />
-      <Button text="teste" onClick={() => console.log(1)}></Button>
+
+      <div className="controls">
+        <Button text="Work" onClick={() => configureWork()}></Button>
+        <Button text="teste" onClick={() => console.log(1)}></Button>
+        <Button
+          text={timeCounting ? 'Pause' : 'Play'}
+          onClick={() => setTimeCounting(!timeCounting)}
+        ></Button>
+      </div>
+      <div className="details">
+        <p>
+          Testando: Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, quibusdam eaque
+          quis harum autem inventore officia iusto dicta voluptates laborum dolores. Repudiandae
+          optio iste facilis eum ipsum porro consequatur consequuntur!
+        </p>
+        <p>
+          Testando: Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, quibusdam eaque
+          quis harum autem inventore officia iusto dicta voluptates laborum dolores. Repudiandae
+          optio iste facilis eum ipsum porro consequatur consequuntur!
+        </p>
+      </div>
     </div>
   );
 }
