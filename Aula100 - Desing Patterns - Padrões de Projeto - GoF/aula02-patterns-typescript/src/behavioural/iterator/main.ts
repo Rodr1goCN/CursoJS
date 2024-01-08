ae0ac9c8a8fa9673097a85327d9fb9c84dbb1933
Rodrigo Costa <drigocn2@hotmail.com>
2024-01-08 20:17:15 -0300

    aula02-patterns-typescript
    adicionando mais coisas no iterator

--- Aula100 - Desing Patterns - Padrões de Projeto - GoF/aula02-patterns-typescript/src/behavioural/iterator/main.ts
+++ Aula100 - Desing Patterns - Padrões de Projeto - GoF/aula02-patterns-typescript/src/behavioural/iterator/main.ts
@@ -6,10 +6,14 @@ dataStructure.addItem('A', 'B', 'C', 'D', 'E', 'F');
 //console.log(dataStructure);
 
 // Se retirarmos o Symbol.itarator, pede para fazer.
-const [a, b, c] = dataStructure;
+const [a, b] = dataStructure;
 
-console.log('ROUBEI:', a, b, c);
+console.log('ROUBEI:', a, b);
+console.log('EXECUTEI VÁRIAS COISAS E DEPOIS USEI O ITERATOR');
+const [c, ...rest] = dataStructure;
+console.log(c, rest);
 
+dataStructure.resetIterator();
 for (const data of dataStructure) {
   console.log(data);
 }
@@ -20,4 +24,11 @@ for (const data of dataStructure) {
   console.log(data);
 }
 
+console.log();
+
+console.log('AQUI PRECISO ZERAR O ITERATOR');
+dataStructure.resetIterator();
+for (const data of dataStructure) {
+  console.log(data);
+}
 console.log();
